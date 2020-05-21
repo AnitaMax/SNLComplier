6aa2f8c7aab817a6555b2d5b37adc5de155bd955
maxzheng <1836941753@qq.com>
2020-05-21 21:56:44 +0800

    LL1语法分析器完成 更新了示例程序

--- src/cn/jlu/edu/ccst/Parsing/Test/Controller/LL1MachineTest.java
+++ src/cn/jlu/edu/ccst/Parsing/Test/Controller/LL1MachineTest.java
@@ -20,32 +20,39 @@ class LL1MachineTest {
         assertFalse(result.isSuccess());
 
         var s="program p\n" +
-                "        type\n" +
-                "            t = integer;\n" +
-                "            a=array[3..5] of integer;\n" +
-                "            vv=record " +
-                "                   integer v3,cd;" +
-                "                end;\n" +
-                "        var\n" +
-                "            t v1;\n" +
-                "            char v2;\n" +
-                "            a v3;\n" +
-                "        {\n" +
-                "        测试注释\n" +
-                "        }\n" +
-                "        procedure test1(integer in1,in2);\n" +
-                "        begin\n" +
-                "        write(in1);\n"+
-                "        in1:=in2\n" +
-                "        end\n" +
+                "type\n" +
+                "    t = integer;\n" +
+                "    a=array[3..5] of integer;\n" +
+                "    vv=record \n" +
+                "        integer v3,cd;\n" +
+                "     end;\n" +
+                "var\n" +
+                "    t v1;\n" +
+                "    char v2;\n" +
+                "    a v3;\n" +
+                "{\n" +
+                "测试注释\n" +
+                "}\n" +
+                "procedure test1(integer in1,in2);\n" +
+                "begin\n" +
+                "    in1:=in2\n" +
+                "end\n" +
                 "\n" +
-                "        begin\n" +
-                "            v2:='1';\n" +
-                "            v2:=vv.cd;\n" +
-                "            read(v1);\n" +
-                "            v1:=v1+10;\n" +
-                "            write(v1)\n" +
-                "        end" ;
+                "begin\n" +
+                "    if i<2 then \n" +
+                "        v2:=3\n" +
+                "    else\n" +
+                "        v2:=4\n" +
+                "    fi;\n" +
+                "    while i<3 do\n" +
+                "        v2:=v2+1\n" +
+                "    endwh;\n" +
+                "    v2:='1';\n" +
+                "    v2:=vv.cd;\n" +
+                "    read(v1);\n" +
+                "    v1:=v1+10;\n" +
+                "    write(v1)\n" +
+                "end" ;
         InfoUtil.initialize();
         TokenUtil.doToken(s);
         var tokens=InfoUtil.tokenList;
